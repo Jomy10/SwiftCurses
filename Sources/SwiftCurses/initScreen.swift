20ba20edc45b5a9e64ab76169f1ed807def7f9d0
Jomy10 <jonas.vbs4@gmail.com>
2023-06-21 10:48:17 +0200

    added async main function support

--- Sources/SwiftCurses/initScreen.swift
+++ Sources/SwiftCurses/initScreen.swift
@@ -26,6 +26,33 @@ public func initScreen(
     try body(&scr)
 }
 
+/// Start a new curses mode terminal
+///
+/// This version of the `initScreen` function supports an asynchronous body function
+///
+/// - Parameters:
+///     - `settings`: NCurses settings
+///     - `body`: The paramater passed to the `body` function can be used to call ncures functions on the screen that was created
+public func initScreenAsync(
+    settings: [TermSetting] = TermSetting.defaultSettings,
+    windowSettings: [WindowSetting] = WindowSetting.defaultSettings,
+    _ body: (inout Window) async throws -> ()
+) async throws {
+    ncurses.setlocale(LC_ALL, "") // support for wide chars
+
+    guard let _scr = initscr() else { // start curses mode
+        throw CursesError(.cannotCreateWindow)
+    }
+    defer {
+        endwin() // end curses mode
+    }
+
+    try settings.forEach { try $0.apply() }
+    windowSettings.forEach { $0.apply(_scr) }
+
+    var scr = Window(_scr)
+    try await body(&scr)
+}
 
 // https://invisible-island.net/ncurses/man/curs_scr_dump.3x.html //
 
