238a75a51dce60154b572afc4fbc33ecad70b0ae
Jomy10 <jonas.vbs4@gmail.com>
2023-06-21 18:38:47 +0200

    added convenience move function
    this function will move the cursor into the
    specified direction
    `WindowProtocol.move(_ diretion: Direction)`
    
    e.g.
    `scr.move(.left)`

--- Sources/SwiftCurses/Window+controls.swift
+++ Sources/SwiftCurses/Window+controls.swift
@@ -1,11 +1,29 @@
 import ncurses
 
+public enum Direction {
+	case up
+	case down
+	case left
+	case right
+}
+
 extension WindowProtocol {
 	/// move the cursor to `row`th row and `col`th column
 	@inlinable
 	public func move(row: Int32, col: Int32) throws {
 		if ncurses.wmove(self.window, row, col) == ERR {
 			throw CursesError(.moveOutsideOfWindow)
+		}
+	}
+
+	@inlinable
+	public func move(_ direction: Direction) throws {
+		let (y, x) = self.yx.tuple
+		switch direction {
+			case .up: try self.move(row: y - 1, col: x)
+			case .down: try self.move(row: y + 1, col: x)
+			case .right: try self.move(row: y, col: x + 1)
+			case .left: try self.move(row: y, col: x - 1)
 		}
 	}
 
